e9c8bef9d289674dae90f6cb8ca60f50b0df73ff
MatthewJA <matthew.alger@gmail.com>
2013-11-18 15:16:41 +1100

    Added gem.coffee

--- compile.bat
+++ compile.bat
@@ -1,3 +1,6 @@
 rem Sass 3.2.12 (Media Mark)
-call sass "src/stylesheets/main.scss" --style compressed > "static/styles.min.css"
-call sass "src/stylesheets/main.scss" --style expanded > "static/styles.css"
+call sass "src\stylesheets\main.scss" --style compressed > "static\styles.min.css"
+call sass "src\stylesheets\main.scss" --style expanded > "static\styles.css"
+
+rem CoffeeScript version 1.6.3
+call coffee -c "src\Graphical-Equation-Manipulator\gem.coffee"
